03bec84b9b38bde750b7ab2c8c55ac35b8cc6cd4
Prashant home <pgole31@gmail.com>
2021-04-10 16:03:02 +0530

    blog added

---
+++ blog/cardiac-rehabilitation/index.php
@@ -0,0 +1,236 @@
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KZQDBZT');</script>
+<!-- End Google Tag Manager -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, shrink-to-fit=no">
+
+    <link rel="alternate" hreflang="en" href="index.php" />
+	<title>Cardiac Rehabilitation and Implications | Find How Can It Help Heal Your Heart</title>
+  <!-- ../../assets/img/blog-heart-attack_mobile.jpg -->
+  <link rel="alternate" hreflang="en" href="https://texascardiology.co/blog/cardiac-rehabilitation" />
+  <meta name="description" content="Cardiac rehab can improve your capability to perform activities of each day residing, lessen your heart disease risk elements and specializes in incrementing cardiovascular patience. Call 469-808-0000 to book an appointment for Cardiac Rehabilitation." />
+  <link rel="canonical" href="https://texascardiology.co/blog/cardiac-rehabilitation" />
+  <meta property="og:locale" content="en_US" />
+  <!--<meta property="og:type" content="article" /> -->
+  <meta property="og:title" content="Cardiac Rehabilitation and Implications | Find How Can It Help Heal Your Heart" />
+  <meta property="og:description" content="Cardiac rehab can improve your capability to perform activities of each day residing, lessen your heart disease risk elements and specializes in incrementing cardiovascular patience. Call 469-808-0000 to book an appointment for Cardiac Rehabilitation." />
+  <meta name="keywords" content="cardiac rehabilitation, cardiovascular disease, cardiologist near me, cardiovascular disease symptoms">
+  <meta property="og:url" content="https://texascardiology.co/blog/cardiac-rehabilitation" />
+  <meta property="og:image" content="https://texascardiology.co/assets/img/hert1.jpg" />
+<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+.img-divider{
+  width: 60%;
+  margin: 0 auto;
+  display: block;
+}
+</style>
+
+<link rel="icon" href="../../assets/img/cropped-favicon-32x32.png" sizes="32x32" />
+<link rel="icon" href="../../assets/img/cropped-favicon-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon" href="../../assets/img/cropped-favicon-180x180.png" />
+
+<link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="../../assets/css/animate.min.css">
+<link rel="stylesheet" href="../../assets/css/animate_modified.css">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/font-awesome-line-awesome/css/all.min.css">
+<link rel="stylesheet" href="../../assets/css/custom.css">
+<link rel="stylesheet" href="../../assets/css/responsive.css">
+<link rel='stylesheet' href='../../assets/css/wpforms-full3a92.css' type='text/css'/>
+<link rel="stylesheet" href="../../assets/css/servic.css" type='text/css'>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-191978858-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-191978858-1');
+</script>
+</head>
+  <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KZQDBZT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+    <div id="blue_filter" style="display:none;"></div>
+
+    <?php include("../../modal.php"); ?>
+
+    <?php include("../../menu.php"); ?>
+
+    <div id="sticky_header_btns" class="d-none d-lg-flex justify-content-center align-items-center">
+      <div id="sticky_header_wrap" class="d-flex justify-content-center align-items-center">
+                  <a href="https://texascardiology.co/">Home</a>
+                    <p>:</p>
+                            <a href="https://texascardiology.co/blog/">Blog</a>
+                        </div>
+    </div>
+
+    <header id="menu-top">
+      <div class="row">
+        <div class="logo col">
+          <a href="https://texascardiology.co/"><img src="../../assets/img/logo.png" alt="logo"></a>
+        </div>
+        <nav class="col-5 text-center d-none d-xl-block">
+          <ul>
+                        <li><a href="https://texascardiology.co/" class="d-flex align-items-center justify-content-center">Home</a></li>
+                         <li>:</li>
+                                    <li><a href="https://texascardiology.co/blog/" class="d-flex align-items-center justify-content-center">Blog</a></li>
+                                   </ul>
+        </nav>
+
+        <div id="demonstration_gratuite" class="col text-right">
+          <a href="#" id="video_controls"><i class="fas fa-volume-up"></i></a>
+          <a href="#" id="main_btn_video" class="d-inline-flex align-items-center justify-content-center" href="#" data-action="open" data-target="#pale_blue_aside" data-code="!demonstration!">Schedule an Appointment</a>
+        </div>
+      </div>
+    </header>
+<main id="pharmacies">
+
+  <section id="hero" class="subpage bg_parallax d-flex justify-content-center justify-content-sm-start align-items-center text-center text-sm-left" data-bg_mobile="../../assets/img/card1.jpg" data-bg_screen="../../assets/img/card1.jpg" style="background-image:url('../../assets/img/card1.jpg')">
+  <div id="hero_titles" class="row rellax" data-rellax-speed="8">
+    <div class="col">
+      <h1 style="font-weight: 700;"> Cardiac Rehabilitation</h1>
+    </div>  
+  </div>
+</section>
+  <section id="solutions-pharmacies_cliniques" class="subpage_presentation">
+    <div class="title-section d-none d-lg-flex justify-content-center align-items-center">
+       Stroke</div>
+    <div class="row">
+      <!-- <div class="col-12 col-lg-6 rellax" data-rellax-speed="-3" data-rellax-percentage="0.2">
+        <p class="title-small">Cardiology </p>
+        <p class="desc-big">
+          ClicHealth’s Pharmaservices module offers an online appointment management solution designed for modern pharmacy practices.        </p>
+      </div> -->
+      <div class="col-12 col-lg-12 rellax ulbul" data-rellax-speed="2" data-rellax-percentage="0.3">
+          <p class="desc-big">What is Cardiac Rehabilitation?</p>
+        <p class="desc-normal">Cardiac rehab is a medically supervised program designed to improve your cardiovascular fitness when you have experienced coronary heart assault, coronary heart failure, angioplasty or coronary heart surgical treatment. Cardiac rehabilitation, additionally known as cardiac rehab, is a customized outpatient application of exercising and education. The program is designed to help you improve your health and get over a coronary heart assault, other types of heart sickness or surgical procedure to deal with heart disorder.</p><br><br>
+
+
+          <p class="desc-big">03 Phases of Cardiac Rehabilitation:</p>
+          <p class="desc-normal blue_check">
+            
+            <span><b>The Acute Phase of Cardiac Rehabilitation</b></span><br /><br />
+            <span><b>Your Outpatient Rehabilitation Program</b></span><br /><br />
+            <span><b>Independent Ongoing Maintenance</b></span>
+          </p>
+
+          <br><br>
+          <p class="desc-normal">Cardiac rehabilitation is a supervised software that includes: Physical hobby. Education about wholesome residing, such as a way to eat healthy, take remedy as prescribed, and stop smoking. Counseling to find ways to alleviate stress and enhance mental fitness.</p><br><br>
+
+          <p class="desc-big">Some of the specific exercises you will complete at this phase include:</p>
+        <p class="desc-normal blue_check">           
+            
+            <span><b>Six-minute walk</b></span><br /><br />
+            <span><b>Timed up and move check, which consists of rising from a chair, strolling a brief distance, and sitting backtrack</b></span><br /><br />
+            <span><b>Biking</b></span><br /><br />
+            <span><b>Rowing</b></span><br /><br />
+            <span><b>Walking on a treadmill</b></span><br /><br />
+            <span><b>Exercises to growth flexibility, top frame power, and decrease frame power</b></span><br /><br />
+          </p><br><br>
+
+          <p class="desc-normal">Cardiac rehabilitation, additionally known as cardiac rehab, is a medically supervised program for humans who have had a coronary heart assault, heart failure, coronary heart valve surgery, coronary artery skip grafting, or percutaneous coronary intervention.</p>
+
+
+        <img src="../../assets/img/card2.jpg" class="img-divider"><br><br>
+        <p class="desc-big">What are the Exercises you do in Cardiac Rehabilitation?</p>
+        <p class="desc-normal">Riding a bike, taking walks on a treadmill, and resistance education (running with weights) are kinds of workout you may do all through cardiac rehabilitation (rehab). You will possibly do aerobic exercising, power schooling, and physical activities to increase your flexibility.</p>
+        <p class="desc-normal">Walking is a vital shape of exercise – it will assist you to make the most of your operation. Space your sports thru the day. Adjust your hobby stage via how you feel. Build up strolling as cautioned.</p><br><br>
+
+
+
+        <p class="desc-big">What Type of Diet You Should Follow?</p>
+        <p class="desc-normal" style="color: #000;">A very low-fat diet, in mixture with smoking cessation, workout, and meditation, may additionally enhance cardiac perfusion and decrease angina signs. The Mediterranean weight loss program confers morbidity and mortality benefits in patients with acknowledged cardiovascular sickness.</p><br><br>
+
+        <p class="desc-big">Is Cardiac Rehabilitation worth it?</p>
+        <p class="desc-normal">Going through cardiac Rehabilitation results in a healthier life-style, because of weight loss, improved muscle tone and electricity, decreased blood pressure, decreased insulin resistance, and advanced lipids. The program enables you stop smoking, lowers your strain stage, and forestalls osteoporosis.</p>
+        <p class="desc-normal">The goals of cardiac rehabilitation encompass establishing a plan to help you regain strength, prevent your condition from worsening, reduce your risk of destiny coronary heart troubles, and enhance your fitness and pleasantness..</p><br><br>
+
+        <img src="../../assets/img/card3.jpg" class="img-divider"><br><br>
+
+        <p class="desc-big">Why it's done:</p>
+        <p class="desc-normal">Cardiac rehabilitation is an option for people with many types of heart ailment. You may benefit from cardiac rehabilitation in case your medical history consists of:</p>
+        <ul>
+          <li><p>Heart attack</p></li>
+          <li><p>Coronary artery disease</p></li>
+          <li><p>Heart failure</p></li>
+          <li><p>Peripheral artery disease</p></li>
+          <li><p>Chest ache (angina)</p></li>
+          <li><p>Cardiomyopathy</p></li>
+          <li><p>Certain congenital heart illnesses</p></li>
+          <li><p>Coronary artery bypass surgical procedure</p></li>
+          <li><p>Angioplasty and stents</p></li>
+          <li><p>Heart or lung transplant</p></li>
+          <li><p>Heart valve repair or substitute</p></li>
+          <li><p>Pulmonary high blood pressure</p></li>
+        </ul><br><br>        
+
+        <br><br>
+        <p class="desc-normal">If you or anyone you know is suffering from a cardiovascular disease, our expert providers at Texas Cardiology will take care of your health and help you recover.</p>
+        <p class="desc-normal">Call on  <a href="tel:469-808-0000">469-808-0000</a> or fill in the below form to <a href="https://texascardiology.co/contact/"><b>book an appointment</b></a> with Dr. Yaqub.</p><br><br>
+
+
+
+
+
+
+
+        
+
+
+
+
+                <!-- <a class="btn-linear_gradient_blue" href="../../about/index.php">About ClicHealth</a> -->
+              </div>
+    </div>
+  </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</main>
+
+    <?php include("../../footer.php"); ?>  
+
+    <script src="../../assets/js/bootstrap-4.4.1.min.js"></script>
+    <script src="../../assets/js/wow.min.js"></script>  
+    <script src="../../assets/js/rellax.min.js"></script>
+    <script src="../../assets/js/custom.js"></script>
+
+
+  </body>
+</html>
